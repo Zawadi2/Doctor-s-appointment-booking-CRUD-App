8b32c6b17b633c2caa1fb98242d57d64bbbc46f5
Zawadi2 <zawadin95@gmail.comm>
2024-07-22 22:28:25 -0500

    firt commit

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -11,8 +11,22 @@ router.get('/', async (req, res) => {
       console.log(error)
       res.redirect('/')
     }
+});
+
+router.get('/new', async (req, res) => {
+    res.render('appointments/new.ejs');
   });
   
-  
+  router.post('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.applications.push(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/appointments`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  }); 
 
 module.exports = router;
