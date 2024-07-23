1ae6d7bf3767c0d5baaa49541b4d2ef22ae36cc3
Zawadi2 <zawadin95@gmail.comm>
2024-07-22 19:29:03 -0500

    fisrt commit

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String, 
+    required: true
+  },
+  Phone: {
+    type: String,
+    required: true
+  },
+  appointmentDate: {
+    type:Date,
+    required: true
+  },
+  appointmentTime: {
+    type: String,
+    required: true
+  }
+})
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  appointments: [appointmentSchema]
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
